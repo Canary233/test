a1103b06e497e432e41a637f457fd808088fff37
tracer-ics2017 <tracer@njuics.org>
2022-04-11 09:51:57 +0800

    >  compile 162030112 niyiwei Linux debian 4.19.0-19-686 #1 SMP Debian 4.19.232-1 (2022-03-07) i686 GNU/Linux  09:51:57 up  1:50,  1 user,  load average: 0.00, 0.00, 0.00 34ec3b20b24f7be4da4b4ffe6eaf2a386787073d

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -8,7 +8,9 @@ typedef struct watchpoint {
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
-
+  uint32_t old_val;
+  uint32_t new_val;
+  char expr[32];
 
 } WP;
 
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -1,6 +1,6 @@
 #include "monitor/watchpoint.h"
 #include "monitor/expr.h"
-
+#include<string.h>
 #define NR_WP 32
 
 static WP wp_pool[NR_WP];
@@ -11,6 +11,8 @@ void init_wp_pool() {
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = &wp_pool[i + 1];
+    wp_pool[i].old_val =0;
+    wp_pool[i].new_val =0;
   }
   wp_pool[NR_WP - 1].next = NULL;
 
@@ -20,4 +22,70 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
-
+void *new_wp(char *st)
+{
+   if(free_==NULL)
+   { 
+      printf("free链表为空！\n");
+      assert(0);      
+   }
+   WP *temp=free_;
+   bool *success=false;
+   free_=free_->next;
+   strcpy(temp->expr,st);
+   temp->old_val=expr(temp->expr,success);
+   if(!success)
+   {
+       printf("运行失败！\n");
+       assert(0);
+   }
+   temp->next=NULL;
+   if(head==NULL)	 
+       head=temp;
+   else
+   {
+       WP *ptemp=head;
+      while(ptemp->next!=NULL)
+	    ptemp=ptemp->next;
+     ptemp->next=temp;  
+   }
+   return temp;
+}
+void free_wp(int n)
+{
+   if(head==NULL)
+   {
+      printf("监视点链表为空！\n");
+      assert(0);      
+   }
+   WP *p=head;
+   WP *p0=head;
+   if(head->NO==n)
+   {
+      head=head->next;
+      p->next=free_;
+      p->old_val=0;
+      p->new_val=0;
+      free_=p;
+   }
+   else
+   {
+      p=p->next;
+      while(p!=NULL)
+      {
+	 if(p->NO==n)
+	 {
+	     p0->next=p->next;
+	     p->old_val=0;
+	     p->new_val=0;
+	     p->next=free_;
+	     free_=p;
+	     printf("Watchpoint %d deleted\n",n);
+	     return;
+	 }
+	 p0=p0->next;
+	 p=p->next;
+      }
+   }
+   printf("Watchpoint %d is not existed\n",n);
+}
