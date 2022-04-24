149814f2a1a21a3d5a672656333d195c29faa5ce
tracer-ics2017 <tracer@njuics.org>
2022-04-24 21:38:47 +0800

    >  compile 162030112 niyiwei Linux debian 4.19.0-19-686 #1 SMP Debian 4.19.232-1 (2022-03-07) i686 GNU/Linux  21:38:47 up  6:51,  1 user,  load average: 0.00, 0.00, 0.00 8b0c7b4a9d3582f068b0e0549a94532d4fbb5ad2

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -132,54 +132,76 @@ static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
 
 static inline void rtl_not(rtlreg_t* dest) {
   // dest <- ~dest
-  TODO();
+  // TODO();
+  *dest=~(*dest);
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+ // TODO();
+  int32_t temp=(int32_t)(*src1);
+  width=width<<3;
+  temp=(temp>>width)<<width;
+  *dest=width;
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
-  TODO();
+  // TODO();
+  src1=src1-4;
+  rtl_sm(&cpu.esp,4,src1);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-  TODO();
+  //TODO();
+  rtl_lm(dest,&cpu.esp,4);
+  cpu.esp=cpu.esp+4;
 }
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 == 0 ? 1 : 0)
-  TODO();
+  //TODO();
+  *dest= (*src1==0) ? 1 : 0;
 }
 
 static inline void rtl_eqi(rtlreg_t* dest, const rtlreg_t* src1, int imm) {
   // dest <- (src1 == imm ? 1 : 0)
-  TODO();
+  //TODO();
+  *dest=(*src1==imm) ? 1 : 0;
 }
 
 static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 != 0 ? 1 : 0)
-  TODO();
+ // TODO();
+  *dest=(*src1!=0) ? 1 : 0;
 }
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-  TODO();
+  //TODO();
+  rtl_shri(dest,src1,width*8-1);
 }
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
-  TODO();
+  //TODO();
+  rtlreg_t temp;
+  rtl_shli(&temp,result,32-8*width);
+  if(temp==0)
+	  cpu.ZF=1;
+  else
+	  cpu.ZF=0;
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  TODO();
+  //TODO();
+  rtlreg_t temp;
+  rtl_msb(&temp,result,width);
+  cpu.SF=temp;
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
