27f8ea33c32eb9c97e2fc64cbf56fdff77199a35
tracer-ics2017 <tracer@njuics.org>
2022-04-27 18:28:51 +0800

    >  compile 162030112 niyiwei Linux debian 4.19.0-19-686 #1 SMP Debian 4.19.232-1 (2022-03-07) i686 GNU/Linux  18:28:51 up  7:46,  1 user,  load average: 0.00, 0.00, 0.00 83e4d96a5fbd71828ef91defa1f7dbaf6c3cfe5

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -36,7 +36,8 @@ make_EHelper(ret) {
  // TODO();
   rtl_pop(&decoding.jmp_eip);
   decoding.is_jmp=1;
-  rtl_add(&decoding.jmp_eip,eip,&id_dest->val);
+  if(decoding.opcode==0xc2)
+    cpu.esp=cpu.esp+id_dest->val;
   print_asm("ret");
 }
 
