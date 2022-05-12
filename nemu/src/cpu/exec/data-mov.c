44fd1668d8a50246d03cc218ed6c975ab107c353
tracer-ics2017 <tracer@njuics.org>
2022-05-12 22:42:34 +0800

    >  compile 162030112 niyiwei Linux debian 4.19.0-19-686 #1 SMP Debian 4.19.232-1 (2022-03-07) i686 GNU/Linux  22:42:34 up  1:58,  1 user,  load average: 0.00, 0.00, 0.00 462fc47eff765144b307208693cee350285551d4

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -31,17 +31,28 @@ make_EHelper(popa) {
 }
 
 make_EHelper(leave) {
-  TODO();
-
+  //TODO();
+  rtl_lr(&id_dest->val,R_EBP,4);
+  rtl_sr(R_ESP,4,&id_dest->val);
+  rtl_pop(&id_dest->val);
+  rtl_sr(R_EBP,4,&id_dest->val);
   print_asm("leave");
 }
 
 make_EHelper(cltd) {
   if (decoding.is_operand_size_16) {
-    TODO();
+    //TODO();
+    rtl_lr_w(&t0,R_AX);
+    rtl_sext(&t0,&t0,2);
+    rtl_shri(&t0,&t0,16);
+    rtl_sr_w(R_DX,&t0);
   }
   else {
-    TODO();
+    //TODO();
+    rtl_lr_l(&t0,R_EAX);
+    rtl_sari(&t0,&t0,31);
+    rtl_shri(&t0,&t0,4);
+    rtl_sr_l(R_EDX,&t0);
   }
 
   print_asm(decoding.is_operand_size_16 ? "cwtl" : "cltd");
@@ -49,10 +60,16 @@ make_EHelper(cltd) {
 
 make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
-    TODO();
+   // TODO();
+   rtl_lr_b(&t0,R_AL);
+   rtl_sext(&t0,&t0,1);
+   rtl_sr_w(R_AX,&t0);
   }
   else {
-    TODO();
+   // TODO();
+   rtl_lr_w(&t0,R_AX);
+   rtl_sext(&t0,&t0,2);
+   rtl_sr_l(R_EAX,&t0);
   }
 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
